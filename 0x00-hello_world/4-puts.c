f83b4594edfa623b54af71e5906847e71c9c2267
arturadamian <arturadamian@gmail.com>
2018-01-18 04:30:13 +0000

    my first C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/**
+ * main - prints a string ""Programming is like building a multilingual puzzle"
+ *
+ *Return: the program  should end with the value 0
+ */
+#include <stdio.h>
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
