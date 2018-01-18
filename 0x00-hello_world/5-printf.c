1f80672c281f45b58ff8fa5fbea3062d2dc3cef3
arturadamian <arturadamian@gmail.com>
2018-01-18 04:46:17 +0000

    c program to print a text

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+ * main - prints "\"Programming is like building a multilingual puzzle\n" with
+ *
+ *Return: the program should return 0
+ */
+#include <stdio.h>
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
