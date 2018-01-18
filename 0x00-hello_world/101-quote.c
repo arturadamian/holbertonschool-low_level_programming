2482538aa9c72cfe52ff6da433d79b14abb93f03
arturadamian <arturadamian@gmail.com>
2018-01-18 07:30:39 +0000

    writing to the stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-#include <stdarg.h>
+#include <unistd.h>
+
 /**
  * main - printing to the standard error
- * Return: Always 0 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	fprintf(stderr,
-		 "and that piece of art is useful\" Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	      , 59);
 	return (1);
 }
