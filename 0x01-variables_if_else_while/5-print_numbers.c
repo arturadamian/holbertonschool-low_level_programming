d25038d8b0ea0571b8b28c70243bd3579d931ff9
arturadamian <arturadamian@gmail.com>
2018-01-19 04:51:18 +0000

    print digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - printing the alphabet
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '0' ; c <= '9' ; c++)
+
+		putchar (c);
+	putchar('\n');
+
+	return (0);
+}
