4195336f7bdffe3efe5f7fc95843e1957cf4bd5e
arturadamian <arturadamian@gmail.com>
2018-01-19 05:41:31 +0000

    digits and commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - printing the alphabet
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c <= 9)
+	{
+			putchar (c + '0');
+
+			if (c != 9)
+				putchar (',');
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
