b0aa0f450480aec73d4ac76d503b537236387a3f
arturadamian <arturadamian@gmail.com>
2018-01-19 06:00:13 +0000

    digits and commas

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,16 +9,18 @@ int main(void)
 	int c = 0;
 
 	while (c <= 9)
-	{
-			putchar (c + '0');
 
-			if (c != 9)
-			{
-				putchar (',');
-				putchar (' ');
-			}
+	{
+		putchar(c + '0');
+
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		c++;
 	}
+
 	putchar('\n');
 
 	return (0);
