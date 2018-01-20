e454afdf39df2e32aeb9ca5c057874c8ba5783bd
arturadamian <arturadamian@gmail.com>
2018-01-20 06:50:10 +0000

    random numbers with conditions

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - printing random numbers with conditions
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = 0; c <= 9; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					if ((a == c && b < d) || a < c)
+					{
+						putchar('0' + a);
+						putchar('0' + b);
+						putchar(' ');
+						putchar('0' + c);
+						putchar('0' + d);
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar ('\n');
+
+	return (0);
+
+}
