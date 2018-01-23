2c796c8a6a504849f531aab7d972b5a6f14c699b
arturadamian <arturadamian@gmail.com>
2018-01-23 19:01:09 +0000

    print with own putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,9 +1,14 @@
 #include "holberton.h"
-
+/**
+ * main - printing a string with _putchar
+ *
+ * Return: 0 (success)
+ */
 int main(void)
 {
 	int s = 0;
 	char h[] = "Holberton";
+
 	while (h[s])
 	{
 		_putchar(h[s]);
