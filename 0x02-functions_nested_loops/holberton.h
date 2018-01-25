57e4a4a4055ca68c86d212c733fe7ced48ec4433
arturadamian <arturadamian@gmail.com>
2018-01-25 00:45:51 +0000

    prints the n times table, starting with 0

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -14,4 +14,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
+void print_times_table(int);
+void spacencomma(int, int);
+void puttimes(int);
 #endif
