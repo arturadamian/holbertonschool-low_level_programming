30bc6cb3a59aefa5cb208a5b109073a11b162b6c
arturadamian <arturadamian@gmail.com>
2018-01-25 05:28:16 +0000

    fix fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main -  prints the first 50 Fibonacci numbers, starting with 1 and 2
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	long int f;
+	long int b = 1;
+	long int c = 2;
+	int a = 0;
+
+	while (a <= 50)
+	{
+		f = b + c;
+		printf("%ld, ", f);
+		b = c;
+		c = f;
+		a++;
+	}
+	return (0);
+}
