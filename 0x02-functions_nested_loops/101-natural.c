19cb052c752194ebed8daed3977d3c9837f39ac9
arturadamian <arturadamian@gmail.com>
2018-01-25 04:57:46 +0000

    fixed

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,13 +8,12 @@
 int main(void)
 {
 	int sum = 0;
-	int i = 0;
+	int i;
 
-	while (i < 1024)
+	for (i = 0; i < 1024; i++)
 	{
-		if (i % 3 == 0 || i % 5 == 0)
+		if ((i % 3 == 0) || (i % 5 == 0))
 			sum += i;
-		i++;
 	}
 	printf("%d\n", sum);
 	return (0);
