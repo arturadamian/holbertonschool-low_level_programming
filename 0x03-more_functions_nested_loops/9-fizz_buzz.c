69131f2216b1cf87799367153f248a7e17bfa779
arturadamian <arturadamian@gmail.com>
2018-01-26 02:53:34 +0000

    prints digits and words

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints the numbers from 1 to 100,
+ * But for multiples of three print Fizz instead of the number
+ * and for the multiples of five print Buzz
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 100; a++)
+	{
+		if (a % 3 == 0)
+			printf("Fizz");
+		else if (a % 5 == 0)
+			printf("Buzz");
+		else if (a % 3 == 0 && a % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", a);
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
