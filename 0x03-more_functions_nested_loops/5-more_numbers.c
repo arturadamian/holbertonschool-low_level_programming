1988cfaf4a5c2424a83670883767e2d578b90fe8
arturadamian <arturadamian@gmail.com>
2018-01-26 00:48:30 +0000

    fix variables

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 
-
 /**
  * more_numbers -  prints 10 times the numbers, from 0 to 14
  */
 void more_numbers(void)
 {
-	int i, c;
+	int i;
+	int c;
 
 	for (i = 0; i <= 10; i++)
 	{
