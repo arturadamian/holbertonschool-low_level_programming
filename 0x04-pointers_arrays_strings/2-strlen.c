89cae73f0d762cb5662d381618eee48b96934dfb
arturadamian <arturadamian@gmail.com>
2018-01-30 19:04:29 +0000

    returns the length of a string.

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+
+/**
+ * _strlen - returns the length of a string.
+ * @*s: first parameter
+ * Return: 0 (success)
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
