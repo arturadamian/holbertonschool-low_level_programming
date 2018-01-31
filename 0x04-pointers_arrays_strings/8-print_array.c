7bcd8aebba08536df05fcf8a4812077512a7cf80
arturadamian <arturadamian@gmail.com>
2018-01-31 00:46:26 +0000

    prints element of an array

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+
+
+/**
+ * print_array - prints elements of an array of integers
+ * @a: pointer ton an array
+ * @n: amount of elements
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n - 1; i++)
+	{
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+}
