9dbfa0a3f3947ac2aaa6b4dda8841be72679e4ca
arturadamian <arturadamian@gmail.com>
2018-01-31 02:16:14 +0000

    prints an array

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -11,11 +11,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i <= n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
-		if (i != n - 1)
+		if (i < n - 1)
 			printf("%d, ", a[i]);
 		else
-			printf("%d", a[i]);
+			printf("%d\n", a[i]);
 	}
 }
