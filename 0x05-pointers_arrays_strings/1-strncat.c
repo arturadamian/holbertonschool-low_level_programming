e9ef216d2a07ed89b487739a8fbb79768f8c3d8c
arturadamian <arturadamian@gmail.com>
2018-02-01 20:56:36 +0000

    concatenates two strings

--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -2,9 +2,11 @@
 
 
 /**
- *
- *
- *
+ * _strncat - concatenates two strings
+ * @dest: pointer to a first string
+ * @src: pointer to a second string
+ * @n: length of src
+ * Return: dest (pointer to the resulting string)
  */
 char *_strncat(char *dest, char *src, int n)
 {
