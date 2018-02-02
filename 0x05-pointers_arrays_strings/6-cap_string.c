736dcacb252a68443b3551775f53ac9a8f951efb
arturadamian <arturadamian@gmail.com>
2018-02-02 04:08:54 +0000

    fix the braces

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,6 @@
 #include "holberton.h"
 
 
-
 /**
  * cap_string - capitalize all the words of the string
  * @s: pointer to a string
@@ -19,10 +18,10 @@ char *cap_string(char *s)
 		    s[i] == '?' || s[i] == '"' || s[i] == '\n' ||
 		    s[i] == '(' || s[i] == ')' || s[i] == '{' ||
 		    s[i] == '}')
-		{	i += 1;
+		{
+			i += 1;
 			if (s[i] > 96 && s[i] < 123)
 				s[i] = s[i] - 32;
-
 			else if (s[i] == ' ' || s[i] == '\t' || s[i] == '.' ||
 				 s[i] == ',' || s[i] == ';' || s[i] == '!' ||
 				 s[i] == '?' || s[i] == '"' || s[i] == '\n' ||
