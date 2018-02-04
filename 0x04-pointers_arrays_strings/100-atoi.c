4f84632d266dac3b0fe4fe11ce288a4ee25c4151
arturadamian <arturadamian@gmail.com>
2018-02-04 22:05:04 +0000

    fixed two loops

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -13,16 +13,19 @@ int _atoi(char *s)
 	int num = 1;
 	unsigned int val = 0;
 
-	while (s[i] < '0' || s[i] > '9')
+	while ((s[i] < '0' || s[i] > '9') && s[i] != '\0')
 	{
 		if (s[i] == '-')
 			num *= -1;
 		i++;
 	}
-	while (s[i] >= '0' && s[i] <= '9')
+	if (s[i] >= '0' && s[i] <= '9')
 	{
-		val = val * 10 + (s[i] - '0');
-		i++;
+		while (s[i] >= '0' && s[i] <= '9')
+		{
+			val = val * 10 + (s[i] - '0');
+			i++;
+		}
 	}
 	return (val * num);
 }
