fb69fd74366361d19afdaaea9e20abdbd083f645
arturadamian <arturadamian@gmail.com>
2018-02-04 06:30:25 +0000

    converts a string to a pointer

---
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: pointer to a string
+ *
+ * Return: the value
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int a = 1;
+	int b = 0;
+	unsigned int c = 1;
+	unsigned int v = 0;
+
+	while (s[i] < '0' || s[i] > '9')
+	{
+		if (s[i] == '-')
+			a *= -1;
+		i++;
+	}
+	if (s[i] >= '0' && s[i] <= '9')
+	{
+		while (s[i] >= '0' && s[i] <= '9')
+		{
+			i++;
+			b++;
+		}
+		i -= 1;
+		while (b > 0)
+		{
+			v += (s[i] - 48) * c;
+			c *= 10;
+			b--;
+			i--;
+		}
+		v *= a;
+	}
+	else
+		return (0);
+	return (v);
+}
