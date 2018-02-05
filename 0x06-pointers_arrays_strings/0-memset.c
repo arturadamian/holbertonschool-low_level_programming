a71beeecd9897ab8c681d6dd5c03d4fe7a0afd50
arturadamian <arturadamian@gmail.com>
2018-02-05 23:57:33 +0000

    fills a memory with a constant byte

---
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+
+/**
+ *
+ *
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a = 0;
+
+	while (a < n)
+	{
+		s[a] = b;
+		a++;
+	}
+	return (s);
+}
