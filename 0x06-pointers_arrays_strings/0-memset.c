dc5db50c361147c2058afcb9db17f41d302822a0
arturadamian <arturadamian@gmail.com>
2018-02-06 00:08:22 +0000

    fix the comments

--- 0x06-pointers_arrays_strings/0-memset.c
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -2,8 +2,12 @@
 
 
 /**
+ * _memset - fills memory with a constant byte
+ * @s: pointer to a string
+ * @b: constant byte
+ * @n: amount of bytes
  *
- *
+ * Return: the result
  */
 char *_memset(char *s, char b, unsigned int n)
 {
