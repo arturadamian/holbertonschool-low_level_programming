f835c8e413afdff7b9ecb65016a673d3bc6559d2
arturadamian <arturadamian@gmail.com>
2018-02-06 23:26:09 +0000

    locates a substring in a string

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -13,6 +13,8 @@ char *_strstr(char *haystack, char *needle)
 	int i;
 	int j;
 
+	if (needle[0] == '\0')
+		return (haystack);
 	for (j = 0; haystack[j] != '\0'; j++)
 	{
 		if (haystack[j] == needle[0])
