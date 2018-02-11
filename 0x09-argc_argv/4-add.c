b420b906834f75f59203a8d7c7db2c2f9b2526e5
arturadamian <arturadamian@gmail.com>
2018-02-11 08:43:45 +0000

    Betty

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -11,18 +11,17 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	int sum = 0;
+	int i, sum = 0;
 
-	for (i = 1; i < argc; i++ )
+	for (i = 1; i < argc; i++)
+	{
+		if (*argv[i] > '9' || *argv[i] < '0')
 		{
-			if (*argv[i] > '9' || *argv[i] < '0')
-			{
-				printf("Error\n");
-				return (1);
-			}
-			sum += atoi(argv[i]);
+			printf("Error\n");
+			return (1);
 		}
+		sum += atoi(argv[i]);
+	}
 	printf("%d\n", sum);
 	return (0);
 }
