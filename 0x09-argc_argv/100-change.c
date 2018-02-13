0eef636ebe85b362d65a51c41749e87639d03682
arturadamian <arturadamian@gmail.com>
2018-02-13 02:25:51 +0000

    fix the Error statement

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -14,21 +14,20 @@ int main(int argc, char *argv[])
 	int i, cents, num = 0;
 	int array[5] = {25, 10, 5, 2, 1};
 
-	cents = atoi(argv[1]);
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	cents = atoi(argv[1]);
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-
 	for (i = 0; i < 5; i++)
 	{
-	num += cents / array[i];
+		num += cents / array[i];
 		cents %= array[i];
 	}
 	printf("%d\n", num);
