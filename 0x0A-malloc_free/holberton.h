861ffc3e60cadd4b8f83779ef224207429afdeef
arturadamian <arturadamian@gmail.com>
2018-02-13 23:54:26 +0000

    header

--- 0x0A-malloc_free/holberton.h
+++ 0x0A-malloc_free/holberton.h
@@ -1,6 +1,13 @@
 #ifndef holberton_h
 #define holberton_h
-y
+
+
+char *argstostr(int ac, char **av);
+void free_grid(int **grid, int height);
+int **alloc_grid(int width, int height);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+char *create_array(unsigned int size, char c);
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
