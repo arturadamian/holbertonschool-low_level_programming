2c8dd8029cadfbe31287cc4ddbf74052291f39ef
arturadamian <arturadamian@gmail.com>
2018-02-14 01:27:49 +0000

    duplicate a string in an allocated memory

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * _strdup - duplicate a string in an allocated memory
+ * @str: pointer to a string
+ *
+ * Return: a pointer if success or NULL
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int x;
+
+	if (str == NULL)
+		return (NULL);
+	for (x = 0; str[x]; x++)
+		;
+	s = malloc(x * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (x = 0; str[x]; x++)
+	{
+		s[x] = str[x];
+	}
+	return (s);
+}
