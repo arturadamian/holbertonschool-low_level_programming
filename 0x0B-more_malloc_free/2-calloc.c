3d9976d6e24eb8736d5f1de37a28fe2f7de0df17
arturadamian <arturadamian@gmail.com>
2018-02-16 18:07:07 +0000

    allocates memory for an array

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: elements
+ * @size: bytes
+ *
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+	char *a;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(size * nmemb);
+	if (p == 0)
+		return (0);
+	a = p;
+	for (i = 0; i < nmemb; i++)
+		a[i] = 0;
+	return (p);
+}
