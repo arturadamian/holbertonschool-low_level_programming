e58067957bd996d3b4314cceeae271b9e683065e
arturadamian <arturadamian@gmail.com>
2018-02-20 02:29:15 +0000

    header file

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a composite data type declaration
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
