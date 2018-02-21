994acc948e09aface7450536528661e697821783
arturadamian <arturadamian@gmail.com>
2018-02-21 07:47:34 +0000

    frees dogs

---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+void free_dog(dog_t *d)
+{
+	if (d == 0)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
