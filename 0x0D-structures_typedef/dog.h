7d2b0c0ef6af8746cd03ec924910ef2a2cece892
arturadamian <arturadamian@gmail.com>
2018-02-21 01:08:17 +0000

    a new name for the type

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -13,6 +13,8 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
 int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
