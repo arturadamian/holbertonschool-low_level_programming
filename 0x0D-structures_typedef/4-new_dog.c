3cca07aaaa40c28ece908412210df22db94ff6a4
arturadamian <arturadamian@gmail.com>
2018-02-21 07:33:51 +0000

    fix free

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -17,16 +17,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	p = malloc(sizeof(dog_t));
 	if (p == NULL)
+	{
+		free(p);
 		return (NULL);
+	}
 	p->name = malloc((strlen(name) + 1) * sizeof(char));
 	if ((p->name) == NULL)
 	{
+		free(p->name);
 		free(p);
 		return (NULL);
 	}
 	p->owner = malloc((strlen(owner) + 1) * sizeof(char));
 	if ((p->owner) == NULL)
 	{
+		free(p->owner);
 		free(p->name);
 		free(p);
 		return (NULL);
