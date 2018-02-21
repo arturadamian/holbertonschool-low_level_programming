86fd7049e6e68d5dfd2bd78617d77a4bdcff307f
arturadamian <arturadamian@gmail.com>
2018-02-21 06:50:59 +0000

    header

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -17,4 +17,6 @@ typedef struct dog dog_t;
 int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+void print_dog(struct dog *d);
 #endif
