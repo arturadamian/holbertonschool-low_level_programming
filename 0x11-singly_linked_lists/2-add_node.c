d8d85a4274549adccb3d754c47870558db8816bd
arturadamian <arturadamian@gmail.com>
2018-03-02 19:03:45 +0000

    dont return if str is NULL

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -16,12 +16,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
-	if (new->str == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	new->len = strlen(str);
+	new->len = strlen(new->str);
 	new->next = *head;
 	*head = new;
 	return (new);
