95eefbedd43e00c37d33f1cec03a9c0575f148b8
arturadamian <arturadamian@gmail.com>
2018-03-06 22:52:41 +0000

    checked the position of a new node

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,6 +26,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if ((count + 1) == idx)
 		{
 			new->next = temp->next;
+			if (temp->next == NULL)
+				return (NULL);
 			temp->next = new;
 			return (new);
 		}
