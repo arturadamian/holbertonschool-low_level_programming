d3c1223af60b0538cb7cc77da556edb3521b5390
arturadamian <arturadamian@gmail.com>
2018-03-07 23:32:07 +0000

    add one more variable

---
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+
+/**
+ * delete_nodeint_at_index - deletes a node
+ * @index: index of a node
+ *
+ * Return: 1 if success or -1
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int count = 0;
+	listint_t *temp, *del;
+
+	if (!head || !(*head))
+		return (0);
+	temp = *head;
+	if (index == 0)
+	{
+		temp = temp->next;
+		free(*head);
+		*head = temp;
+		return (1);
+	}
+	for (count = 1; count <= index && (temp); count++)
+		temp = temp->next;
+	if (count == index && temp->next != NULL)
+	{
+		del = temp->next;
+		temp->next = temp->next->next;
+		free(del);
+		return (1);
+	}
+	return (-1);
+}
