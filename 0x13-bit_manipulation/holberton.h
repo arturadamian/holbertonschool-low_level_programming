0aeceb1b33dad673b25fa176e65755011899052f
arturadamian <arturadamian@gmail.com>
2018-03-09 01:02:28 +0000

    header

---
+++ 0x13-bit_manipulation/holberton.h
@@ -0,0 +1,20 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} array;
+int _putchar(char s);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+
+#endif
