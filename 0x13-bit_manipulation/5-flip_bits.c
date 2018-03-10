8216ec43181509be38255d2a324a49aaaf3c6375
arturadamian <arturadamian@gmail.com>
2018-03-10 00:00:31 +0000

    how many bits do we use to flip numbers

---
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+
+/**
+ * flip_bits - counts how many bits you need to change the num
+ * @n: first number
+ * @m: second number
+ *
+ * Return: the sum of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int c;
+	int sum = 0;
+
+	c = n ^ m;
+	while (c > 0)
+	{
+		sum += c & 1;
+		c >>= 1;
+	}
+	return (sum);
+}
