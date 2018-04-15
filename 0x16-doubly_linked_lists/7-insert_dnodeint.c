afa9adcad7d58583ce3f6d16511b1c4855d0241f
arturadamian <arturadamian@gmail.com>
2018-04-15 05:51:39 +0000

    fix the if statement

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *temp;
 	unsigned int c = 0;
 
-	if (!h || !*h)
+	if (!h)
 		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
