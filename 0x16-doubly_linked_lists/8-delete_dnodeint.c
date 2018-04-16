d18b5bf1bcf9ebe748e762a7788b6f44d07ae0a6
arturadamian <arturadamian@gmail.com>
2018-04-16 02:09:09 +0000

    free at the very end

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -13,29 +13,25 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *current;
 	unsigned int c;
 
-	if (!head)
+	if (!head || !*head)
 		return (-1);
 	current = *head;
-	if (index == 0 && current)
+	if (index == 0)
 	{
 		*head = current->next;
+		if (current->next)
+			current->next->prev = NULL;
 		free(current);
 		return (1);
 	}
-	else if (index == 0 && !current)
-		return (-1);
 	for (c = 0; current && c < index; c++)
 		current = current->next;
 	if (c == index)
 	{
-		if (current->next && c == index)
-		{
-			current->prev->next = current->next;
-			if (current->next)
-				current->next->prev = current->prev;
-			free(current);
-			return (1);
-		}
+		current->prev->next = current->next;
+		if (current->next)
+			current->next->prev = current->prev;
+		free(current);
 		return (1);
 	}
 	return (-1);
