50e632e89760bb93e0905106deaffe7c2906b3ec
arturadamian <arturadamian@gmail.com>
2018-05-11 20:42:35 +0000

    creates a new table

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -8,18 +8,16 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i;
 	hash_table_t *table;
-	hash_node_t *node;
 
+	if (size == 0)
+		return (NULL);
 	table = malloc(sizeof(hash_table_t) * size);
 	if (table == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++)
-	{
-		node = malloc(sizeof(char *) * 2);
-		if (node == NULL)
-			return (NULL);
-	}
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t) * size);
+	if (table->array == NULL)
+		return (NULL);
 	return (table);
 }
