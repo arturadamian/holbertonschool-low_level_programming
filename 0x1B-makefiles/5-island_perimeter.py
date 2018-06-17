55b7beee260ff7cc9b65e6b4e90204c9d19a1c14
Artur Adamian <arturadamian@gmail.com>
2018-06-17 15:40:10 -0700

    returns the perimeter of the island described in grid

---
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+def cell_perim(i, j, grid):
+    """return perimeter of cell"""
+    count = 4
+    if grid[i][j + 1] == 1:
+        count -= 1
+    if grid[i][j - 1] == 1:
+        count -= 1
+    if grid[i + 1][j] == 1:
+        count -= 1
+    if grid[i - 1][j] == 1:
+        count -= 1
+    return count
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island"""
+    perim = 0
+    for i in range(1, (len(grid) - 1)):
+        for j in range(1, (len(grid[0]) - 1)):
+            if grid[i][j] == 1:
+                perim += cell_perim(i, j, grid)
+    return perim
