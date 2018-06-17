e719e25ac4cbec53a1ee6834a0b51d73b7c7e4e2
Artur Adamian <arturadamian@gmail.com>
2018-06-17 15:43:14 -0700

    check if grid

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -18,8 +18,9 @@ def cell_perim(i, j, grid):
 def island_perimeter(grid):
     """returns the perimeter of the island"""
     perim = 0
-    for i in range(1, (len(grid) - 1)):
-        for j in range(1, (len(grid[0]) - 1)):
-            if grid[i][j] == 1:
-                perim += cell_perim(i, j, grid)
+    if grid:
+        for i in range(1, (len(grid) - 1)):
+            for j in range(1, (len(grid[0]) - 1)):
+                if grid[i][j] == 1:
+                    perim += cell_perim(i, j, grid)
     return perim
