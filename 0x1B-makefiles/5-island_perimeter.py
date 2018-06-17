762e1404bf6b0e2d6be5dbe7c5ea3173aeae5c30
Artur Adamian <arturadamian@gmail.com>
2018-06-17 16:56:05 -0700

    grid surrounded by water not an island ... should start range

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -5,22 +5,18 @@
 def cell_perim(i, j, grid):
     """return perimeter of cell"""
     count = 4
-    if grid[i][j + 1] == 1:
-        count -= 1
-    if grid[i][j - 1] == 1:
-        count -= 1
-    if grid[i + 1][j] == 1:
-        count -= 1
-    if grid[i - 1][j] == 1:
-        count -= 1
+    if i > 0 and grid[i - 1][j] == 1:
+        count -= 2
+    if j > 0 and grid[i][j - 1] == 1:
+        count -= 2
     return count
 
 
 def island_perimeter(grid):
     """returns the perimeter of the island"""
     perim = 0
-    for i in range(1, (len(grid) - 1)):
-        for j in range(1, (len(grid[0]) - 1)):
+    for i in range(0, len(grid)):
+        for j in range(0, len(grid[0])):
             if grid[i][j] == 1:
                 perim += cell_perim(i, j, grid)
-    return perim if perim > 0 else None
+    return perim
