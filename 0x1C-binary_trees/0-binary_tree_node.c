6a5cac1054930da2e7a9370f598bcbefcdec32e2
Artur Adamian <arturadamian@gmail.com>
2018-07-05 00:51:39 -0700

    swap parent and value places?

--- 0x1C-binary_trees/0-binary_tree_node.c
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -14,7 +14,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	tree = malloc(sizeof(binary_tree_t));
 	if (tree == NULL)
 		return (NULL);
-	tree->parent = parent;
 	tree->n = value;
+	tree->parent = parent;
+	tree->left = NULL;
+	tree->right = NULL;
 	return (tree);
 }
