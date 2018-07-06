7835c533ba59b8efd49e47e96631ef0c44418b07
Artur Adamian <arturadamian@gmail.com>
2018-07-06 09:25:28 -0700

    checking if parent is NULL

--- 0x1C-binary_trees/10-binary_tree_depth.c
+++ 0x1C-binary_trees/10-binary_tree_depth.c
@@ -11,8 +11,9 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t x = 0;
 
-	if (!tree || (tree->left && tree->right))
+	if (!tree || !tree->parent)
 		return (0);
-	x = binary_tree_depth(tree->parent);
-	return (x + 1);
+	if (tree->parent)
+		x =  (binary_tree_depth(tree->parent) + 1);
+	return (x);
 }
