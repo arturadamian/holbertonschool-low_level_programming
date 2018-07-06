01f00a3682df2051cd2154b24cebeac37861c1bc
Artur Adamian <arturadamian@gmail.com>
2018-07-06 09:42:39 -0700

    check if root is NULL

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -2,13 +2,15 @@
 
 
 /**
+ * binary_tree_is_full - checks if the binary tree is full
+ * @tree: pointer to a binary tree
  *
- *
- *
- *
+ * Return: return 1 if full or 0
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
 	if (!tree->left && !tree->right)
 		return (1);
 	if (tree && (tree->left && tree->right))
