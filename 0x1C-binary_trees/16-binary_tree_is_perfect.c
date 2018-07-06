2af2be42a7a3c552ded1c94bf0d9f0fa3801a0d7
Artur Adamian <arturadamian@gmail.com>
2018-07-06 10:42:17 -0700

    need to compare size as well

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -19,17 +19,31 @@ int branch_height(const binary_tree_t *tree)
 	return (y + 1);
 }
 /**
+ * binary_size - function that measures the size of a binary tree
+ * @tree: pointer to the root node
+ * Return: height or 0
+ */
+size_t binary_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (binary_size(tree->left) + 1 +
+		binary_size(tree->right));
+}
+/**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: pointer to a root node
  *
  * Return: 1 if perfect or 0
  */
+
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
 	if (tree && (tree->right && tree->left))
-		if (branch_height(tree->right) == branch_height(tree->left))
+		if (branch_height(tree->right) == branch_height(tree->left) &&
+			(binary_size(tree->left) == binary_size(tree->right)))
 			return (1);
 	return (0);
 }
