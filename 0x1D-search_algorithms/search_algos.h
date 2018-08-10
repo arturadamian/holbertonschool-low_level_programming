3e8cca16f1d12af1b89eccc9c33796897d698a30
arturadamian <arturadamian@gmail.com>
2018-08-10 04:01:48 +0000

    typo - void

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -6,6 +6,6 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int binary_search_(int *array, size_t l, size_t r, int value);
-int print_array(int *array, size_t l, size_t r);
+void print_array(int *array, size_t l, size_t r);
 
 #endif
