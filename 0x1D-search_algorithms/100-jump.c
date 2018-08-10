12ee0748876b0cabfd79c44e849f26f0f4365eaa
arturadamian <arturadamian@gmail.com>
2018-08-10 17:46:17 +0000

    checked everything

--- 0x1D-search_algorithms/100-jump.c
+++ 0x1D-search_algorithms/100-jump.c
@@ -12,27 +12,28 @@ int jump_search(int *array, size_t size, int value)
 {
 	unsigned int jump = 0, left = 0, right = 0;
 
-	if (!array)
-		return (-1);
-	jump = sqrt(size);
-	while (left < size && array[left] <= value)
+	if (array)
 	{
-		printf("Value checked array[%d] = [%d]\n", left, array[left]);
-		right = min(size - 1, left + jump);
-		if (array[left] <= value && array[right] >= value)
-			break;
-		left += jump;
-	}
-	if (left >= size || array[left] > value)
-		return (-1);
-	right = min(size - 1, right);
-	printf("Value found between indexes [%d] and [%d]\n", left, right);
-	while (left <= right && array[left] <= value)
-	{
-		printf("Value checked array[%d] = [%d]\n", left, array[left]);
-		if (array[left] == value)
+		jump = sqrt(size);
+		while (left < size && array[left] <= value)
+		{
+			printf("Value checked array[%d] = [%d]\n", left, array[left]);
+			right = min(size - 1, left + jump);
+			if (array[left] <= value && array[right] >= value)
+				break;
+			left += jump;
+		}
+		if (left >= size || array[left] > value)
+			return (-1);
+		right = min(size - 1, right);
+		printf("Value found between indexes [%d] and [%d]\n", left, right);
+		while (left <= right && array[left] <= value)
+		{
+			printf("Value checked array[%d] = [%d]\n", left, array[left]);
+			if (array[left] == value)
 			return (left);
-		left++;
+			left++;
+		}
 	}
 	return (-1);
 }
